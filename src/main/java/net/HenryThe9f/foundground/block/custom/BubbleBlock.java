578ad3cb67a1a82124453b1b73a028345a10d1bc
HenryThe9f <henry@brown-ri.name>
2026-05-06 23:42:25 -0400

    almost there

--- src/main/java/net/HenryThe9f/foundground/block/custom/BubbleBlock.java
+++ src/main/java/net/HenryThe9f/foundground/block/custom/BubbleBlock.java
@@ -3,6 +3,9 @@ package net.HenryThe9f.foundground.block.custom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -26,6 +29,12 @@ public class BubbleBlock extends HalfTransparentBlock{
 
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        if(!pLevel.isClientSide) {
+            ItemStack itemstack = new ItemStack(this.asItem());
+            ItemEntity pEntity = new ItemEntity(pLevel, pPos.getCenter().x, pPos.getCenter().y, pPos.getCenter().z, itemstack);
+            pLevel.addFreshEntity(pEntity);
+
+        }
         pLevel.setBlockAndUpdate(pPos, Blocks.AIR.defaultBlockState());
         super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
     }
